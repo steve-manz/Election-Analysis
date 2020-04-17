eab28cd561756d3c3c35b4b01f0c3207dc735f6f
steve-manz <59142513+steve-manz@users.noreply.github.com>
2020-04-17 13:41:00 -0500

    Adding PyPoll.py file

---
+++ Python_practice.py
@@ -0,0 +1,34 @@
+# Practice Exercises
+
+#What is the score?
+
+counties = ["Arapahoe","Denver","Jefferson"]
+
+counties_tuple = ("Arapahoe", "Denver", "Jefferson")
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+
+print(message_to_candidate)
+
+for county, voters in counties_dict.items():
+    print (f"{county} has  {voters} registered voters.")
+
+for county_dict in voting_data.item():
+    print (f"{county(0)} has {voters(1)} registered voters")
+
+dir({'Arapahoe': 422829, 'Denver': 463353, 'Jefferson': 432438})
+
+
+
+
